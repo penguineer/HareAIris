e3cc88cc756f29bcbe2d87904f4006087f397429
Stefan Haun <mail@tuxathome.de>
2024-10-30 10:43:30 +0100

    Refine the API towards OpenAI use cases
    Drop the Request ID, as there is already a Correlation ID and return
    the number of tokens processed in input and output.

--- src/main/java/com/penguineering/hareairis/model/ChatRequest.java
+++ src/main/java/com/penguineering/hareairis/model/ChatRequest.java
@@ -1,19 +1,17 @@
 package com.penguineering.hareairis.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ChatRequest {
-    @JsonProperty("request_id")
-    private UUID requestId;
-
     @JsonProperty("message")
     private String message;
 }
